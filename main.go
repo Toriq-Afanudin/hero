f911692294476611dd03672279f008488b4a8912
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-06-15 21:43:13 +0700

    update

--- main.go
+++ main.go
@@ -1,36 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	// "database/sql"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 
 	"heroku.com/controller"
-	// "heroku.com/model"
+	"heroku.com/model"
 )
 
 func main() {
 	r := gin.Default()
-	// db := model.SetupModels()
-	// r.Use(func(c *gin.Context) {
-	// 	c.Set("db", db)
-	// 	c.Next()
-	// })
+	db := model.SetupModels()
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	})
 	r.GET("/", controller.Utama)
 	r.GET("/user", controller.GetUser)
 	r.GET("/login", controller.Login)
 
-	godotenv.Load()
-	ip := os.Getenv("IP")
+	// godotenv.Load()
+	// ip := os.Getenv("IP")
 	port := os.Getenv("PORT")
-	address := fmt.Sprintf("%s:%s", ip, port)
-	fmt.Println(address)
+	if port == "" {
+		port = "8000"
+	}
 
-	r.Run(address)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+	// address := fmt.Sprintf("%s:%s", ip, port)
+	// fmt.Println(address)
+
+	r.Run()
 
 	// r.Run()
 }
