3e8e811d8a84f0cd4cef60f224a086f641e24762
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-06-29 07:20:36 +0700

    Menambahkan CORS Middleware

--- main.go
+++ main.go
@@ -121,6 +121,21 @@ func main() {
 		TimeFunc: time.Now,
 	})
 
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	})
+
 	if err != nil {
 		log.Fatal("JWT Error:" + err.Error())
 	}
