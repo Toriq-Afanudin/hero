df0b51b7fe7d63c81c17550dd5cccf0690f59a15
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-07-21 22:18:33 +0700

    update

--- controller/data_dokter_lihat.go
+++ controller/data_dokter_lihat.go
@@ -6,12 +6,39 @@ import (
 	"heroku.com/model"
 )
 
+type dok struct {
+	Id             int
+	Nama           string
+	Sip            string
+	Jenis_kelamin  string
+	Jadwal_praktek string
+	Poli           string
+	Nomor_telfon   string
+	Nomor_str      string
+}
+
 func Data_dokter_lihat(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 	var dokter []model.Dokter
 	db.Raw("select * from dokters order by id_user desc").Scan(&dokter)
+	var GetDokter []interface{}
+	for i := 0; i < len(dokter); i++ {
+		var user model.User
+		db.Where("id = ?", dokter[i].Id_user).Find(&user)
+		new := dok{
+			Id:             dokter[i].Id_user,
+			Nama:           user.Nama,
+			Sip:            dokter[i].Sip,
+			Jenis_kelamin:  dokter[i].Jenis_kelamin,
+			Jadwal_praktek: dokter[i].Jadwal_praktek,
+			Poli:           dokter[i].Poli,
+			Nomor_telfon:   dokter[i].Nomor_telfon,
+			Nomor_str:      dokter[i].Nomor_str,
+		}
+		GetDokter = append(GetDokter, new)
+	}
 	c.JSON(200, gin.H{
 		"code": 200,
-		"data": dokter,
+		"data": GetDokter,
 	})
 }
--- controller/data_perawat_lihat.go
+++ controller/data_perawat_lihat.go
@@ -6,12 +6,41 @@ import (
 	"heroku.com/model"
 )
 
+type per struct {
+	Id            int
+	Nama          string
+	Sip           string
+	Jenis_kelamin string
+	Poli          string
+	Jadwal_kerja  string
+	Jabatan       string
+	Nomor_telfon  string
+	Nomor_str     string
+}
+
 func Data_perawat_lihat(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 	var perawat []model.Perawat
 	db.Raw("select * from perawats order by id_user desc").Scan(&perawat)
+	var GetPerawat []interface{}
+	for i := 0; i < len(perawat); i++ {
+		var user model.User
+		db.Where("id = ?", perawat[i].Id_user).Find(&user)
+		new := per{
+			Id:            perawat[i].Id_user,
+			Nama:          user.Nama,
+			Sip:           perawat[i].Sip,
+			Jenis_kelamin: perawat[i].Jenis_kelamin,
+			Poli:          perawat[i].Poli,
+			Jadwal_kerja:  perawat[i].Jadwal_kerja,
+			Jabatan:       perawat[i].Jabatan,
+			Nomor_telfon:  perawat[i].Nomor_telfon,
+			Nomor_str:     perawat[i].Nomor_str,
+		}
+		GetPerawat = append(GetPerawat, new)
+	}
 	c.JSON(200, gin.H{
 		"code": 200,
-		"data": perawat,
+		"data": GetPerawat,
 	})
 }
